233b465bee1a152bc4a1d5ac39f5c65da8d36a48
naturenomad <76237641+naturenomad@users.noreply.github.com>
2021-04-09 15:17:23 +0200

    Create exercism.py

---
+++ exercism.py
@@ -0,0 +1,25 @@
+# https://exercism.io/tracks/python/exercises
+
+# Exercises on the Python track
+# 117 Python exercises in total
+
+###########################################
+# 1 - Hello World (easy)
+###########################################
+
+# The classical introductory exercise. Just say "Hello, World!"
+# conditionals, optional values, text formatting
+
+print("Hello, World!")
+
+
+###########################################
+# 2 - Two Fer (easy)
+###########################################
+
+# Create a sentence of the form "One for X, one for me."
+# conditionals, optional values, text formatting
+
+x=input()
+print("One for {}, one for me".format(x))
+
