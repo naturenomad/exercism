4248f2cafb21344ab9eecf57a2dbbfeaa6e9aead
naturenomad <76237641+naturenomad@users.noreply.github.com>
2021-04-16 06:44:43 +0200

    Update exercism.py

--- exercism.py
+++ exercism.py
@@ -183,4 +183,34 @@ wordcount("This is not a pig, this is a mouse")
 
 
 
+###########################################
+# 10 - Scrabble Score (easy)
+###########################################
+
+# Given a word, compute the Scrabble score for that word.
+
+# games, loops, maps, strings
+
+# https://scrabble.hasbro.com/en-us/faq
+#    (1 point)-A, E, I, O, U, L, N, S, T, R
+#    (2 points)-D, G
+#    (3 points)-B, C, M, P
+#    (4 points)-F, H, V, W, Y
+#    (5 points)-K
+#    (8 points)- J, X
+#    (10 points)-Q, Z
+
+def scrabblescore(word):
+    scoredict = {'a':1, 'b':3, 'c':3, 'd':2, 'e':1, 'f':4, 'g':2, 'h':4, 'i':1, 'j':8, 'k':5, 'l':1, 'm':3, 
+        'n':1, 'o':1, 'p':3, 'q':10, 'r':1, 's':1, 't':1, 'u':1, 'v':4, 'w':4, 'x':8, 'y':4, 'z':10}
+    score = 0
+    for letter in word:
+        score += scoredict[letter]
+    print(score)
+
+scrabblescore('blingblong')
+scrabblescore('hi')
+
+
+
 
